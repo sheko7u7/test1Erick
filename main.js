8a6e3fc3cdaf0b351408b88395f0974727740d3f
sergio farid morales garcia <chekitho.lmfao@gmail.com>
2024-05-16 08:05:56 -0600

    Commit desde el local

--- main.js
+++ main.js
@@ -3,3 +3,22 @@ function saludarUsuario(nombre) {
 }
 
 saludarUsuario("Usuario");
+
+function crearBotonPeligro() {
+    // Crear el elemento de botón
+    var boton = document.createElement("button");
+    
+    // Asignar texto al botón
+    boton.textContent = "Peligro";
+    
+    // Agregar clase CSS al botón
+    boton.classList.add("btn-peligro");
+    
+    // Agregar el botón al cuerpo del documento
+    document.body.appendChild(boton);
+  }
+  
+  // Llamar a la función para crear el botón de peligro cuando se cargue la página
+  window.onload = function() {
+    crearBotonPeligro();
+  };
